429a34574e1631364dc65fcce756d38f3c8dc9cb
Aldo <aldoleandrosaavedramorales@gmail.com>
2020-08-03 22:40:59 -0400

    ultimas modificaciones a la githubpage

--- docs/bulma.js
+++ docs/bulma.js
@@ -13,21 +13,4 @@ function activo() {
     let isTrue = this == botonLogin || this == modalBg || this == modalClose;
 
     isTrue ? modalLogin.classList.toggle('is-active') : hamburguesa.classList.toggle('is-active'),navmenu.classList.toggle('is-active')
-=======
-const hamburguesa = document.querySelector('.navbar-burger');
-const navmenu = document.querySelector('.navbar-menu');
-const botonLogin = document.getElementById('botonLogin'); // Boton iniciar sesion
-const modalLogin = document.getElementById('modalLogin'); // Modal
-const modalBg = document.querySelector('.modal-background'); // BG
-const modalClose = document.querySelector('.modal-close'); // x
-
-[hamburguesa,botonLogin,modalBg,modalClose].forEach(element => {
-    element.addEventListener('click', activo, true);
-})
-
-function activo() {
-    let isTrue = this == botonLogin || this == modalBg || this == modalClose;
-
-    isTrue ? modalLogin.classList.toggle('is-active') : hamburguesa.classList.toggle('is-active'),navmenu.classList.toggle('is-active')
->>>>>>> 550366efba4cdf6f726d8ee98e8a72fc918f46fd
 }
